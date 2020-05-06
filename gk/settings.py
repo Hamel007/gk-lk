d14aa75b6b26d74c541b0f84bc3f1ce8a3a78980
dimmoon69 <pldimitry@yandex.ru>
2020-05-06 15:21:36 +0300

    Изменил settings

--- gk/settings.py
+++ gk/settings.py
@@ -102,12 +102,7 @@ SITE_ID = 1
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR]
 
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Django-allauth
